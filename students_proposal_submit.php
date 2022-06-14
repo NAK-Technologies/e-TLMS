91d34d9ffc01e4a3aaf8cb77d2d98482d8110d5c
Nehal Ahmed Khan <nak.work.dev@gmail.com>
2022-06-15 01:40:59 +0500

    Bug fixed

--- students_proposal_submit.php
+++ students_proposal_submit.php
@@ -7,10 +7,10 @@ if (isset($_POST['submit'])) {
      $s3 = $_POST['s3_id'];
      $s4 = $_POST['s4_id'];
      $s5 = $_POST['s5_id'];
-     if (!isset($s4)) {
+     if ($s4 == '') {
           $s4 = 0;
      }
-     if (!isset($s5)) {
+     if ($s5 == '') {
           $s5 = 0;
      }
      $name = $_POST['name'];
@@ -22,7 +22,7 @@ if (isset($_POST['submit'])) {
      $location = "uploads/projects/" . $_FILES["proposal"]["name"];
      // echo ($location);
 
-     $q = "INSERT INTO fyp_proposal (student1_id, student2_id, student3_id, student4_id, student5_id, project_name, proposal_file) values ('$s1','$s2','$s3','$s4','$s5','$name','$location')";
+     $q = "INSERT INTO fyp_proposal (student1_id, student2_id, student3_id, student4_id, student5_id, project_name, proposal_file) values ($s1,$s2,$s3,$s4,$s5,'$name','$location')";
 
      if (mysqli_query($conn, $q)) {
           echo 'done';
